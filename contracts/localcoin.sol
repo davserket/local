fafeeaba9712602ced2d0ea2b10f8225bc91fa5f
davserket <cristsegu@gmail.com>
2022-08-16 00:44:35 +0000

    comenzando

--- contracts/localcoin.sol
+++ contracts/localcoin.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.3/contracts/token/ERC20/ERC20.sol";
 
-contract localcoin is ERC721 {
-    constructor() ERC721("localcoin", "LOC") {
+contract local is ERC20 {
+    constructor() ERC20("localcoin", "LOC") {
         _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
     }
 }
